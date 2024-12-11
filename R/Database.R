92a900465d55241b4be2a01a7070e38f32333dd9
dtc-inv <asotolongo@diversifiedtrust.com>
2024-12-10 20:51:23 -0500

    Add files via upload

---
+++ R/Database.R
@@ -0,0 +1,82 @@
+#' Database Object
+#'
+#' @description
+#'   Download and store holdings and price time-series data in s3 from various
+#'   sources such as factset, blackdiamond, excel
+#' @export
+db <- R6::R6Class(
+  'db',
+  public = list(
+    #' @description list of api keys
+    api_keys = NULL,
+    #' @description aws s3 bucket
+    bucket = NULL,
+    #' @description ArcticDB object store
+    ac = NULL,
+    #' @description master security list: table of meta data for all investments
+    tbl_msl = NULL,
+    #' @description custodian list: table of meta data for CTFs and SMAs
+    tbl_cust = NULL,
+    #' @description SEC list: table of meta data for mutual funds and ETFs
+    tbl_sec = NULL,
+    #' @description excel model list: table of meta data for model portfolios
+    tbl_xl_mod = NULL,
+    #' @decription holdings fields: table of fields / columns for holdings data
+    tbl_hold_field = NULL,
+
+    #' @description Create a db object
+    #' @param api_keys list of api keys or `.RData` file location to load the list
+    #'   of keys
+    #' @param py_loc optional file path where python is installed to be used
+    #'   with `reticulate`
+    initialize = function(api_keys,
+      py_loc = "C:/Users/asotolongo/AppData/Local/anaconda3") {
+
+      if (!is.list(api_keys)) {
+        rdat_file <- grep(".RData", api_keys, fixed = TRUE)
+        if (length(rdat_file) == 0) {
+          rdat_file <- FALSE
+        }
+        if (rdat_file) {
+          load(api_keys)
+        }
+      }
+      self$check_api_keys(api_keys)
+      self$api_keys <- api_keys
+      bucket <- arrow::s3_bucket(
+        "dtc-inv",
+        access_key = api_keys$s3$access_key,
+        secret_key = api_keys$s3$secret_key
+      )
+      adb <- try(import("arcticdb"))
+      if ("try-error" %in% class(adb)) {
+        use_python(py_loc)
+        adb <- try(import("arcticdb"))
+        if ("try-error" %in% class(adb)) {
+          return("could not import arcticdb")
+        }
+      }
+      base_url <- 's3://s3.us-east-1.amazonaws.com:dtc-inv?'
+      s3_url <- paste0(
+        base_url,
+        "access=", api_keys$s3$access_key,
+        "&secret=", api_keys$s3$secret_key
+      )
+      ac <- adb$Arctic(s3_url)
+      self$bucket <- bucket
+      self$ac <- ac
+    },
+
+    #' @description Check if api keys are properly specified
+    #' @param api_keys list of api_keys
+    check_api_keys = function(api_keys) {
+      if (all(c("s3", "fs") %in% names(api_keys))) {
+        return(api_keys)
+      } else {
+        stop("s3 and/or fs not found in api_keys")
+      }
+    }
+
+  )
+)
+
---
+++ R/download_data.R
@@ -0,0 +1,171 @@
+download_bd <- function(account_id, api_keys, as_of = NULL) {
+  if (is.null(as_of)) {
+    as_of <- last_us_trading_day()
+  }
+  bd_key <- api_keys$bd_key
+  response <- httr::POST(
+    'https://api.blackdiamondwealthplatform.com/account/Query/HoldingDetailSearch',
+    accept_json(),
+    add_headers(
+      Authorization = paste0('Bearer ', bd_key$refresh_token),
+      `Ocp-Apim-Subscription-Key` = bd_key$bd_subkey
+    ),
+    encode = 'json',
+    body = list(accountID = account_id, asOf = as_of,
+                onlyCurrentHoldings = TRUE, limit = 100000,
+                include = list(returnInfo = TRUE, assets = TRUE))
+  ) # end POST
+  if (response$status_code != 200) {
+    bd_key <- refresh_bd_key(api_keys, TRUE)
+    response <- httr::POST(
+      'https://api.blackdiamondwealthplatform.com/account/Query/HoldingDetailSearch',
+      accept_json(),
+      add_headers(
+        Authorization = paste0('Bearer ', bd_key$refresh_token),
+        `Ocp-Apim-Subscription-Key` = bd_key$bd_subkey
+      ),
+      encode = 'json',
+      body = list(accountID = account_id, asOf = as_of,
+                  onlyCurrentHoldings = TRUE, limit = 100000,
+                  include = list(returnInfo = TRUE, assets = TRUE))
+    ) # end POST
+  }
+  # parse jason and extract data
+  rd <- parse_json(response)
+  rd <- rd$data
+  if (length(rd) == 0) {
+    warning('no data found')
+    return(NULL)
+  }
+  # convert json list into data.frame by looping through each holding
+  df <- data.frame(
+    assetId = NA,
+    name = NA,
+    cusip = NA,
+    ticker = NA,
+    identifier = NA,
+    units = NA,
+    emv = NA,
+    returnInfo = NA
+  )
+  robcheck <- function(x, l) {
+    if (x %in% names(l)) {
+      if (!is.null(l[[x]])) {
+        return(l[x])
+      } else {
+        return(NA)
+      }
+    } else {
+      return(NA)
+    }
+  }
+  for (i in 1:length(rd)) {
+    df[i, 'assetId'] <- rd[[i]]$asset['assetId'][[1]]
+    df[i, 'name'] <- rd[[i]]$asset['name'][[1]]
+    df[i, 'cusip'] <- robcheck('cusip', rd[[i]]$asset)
+    df[i, 'ticker'] <- robcheck('ticker', rd[[i]]$asset)
+    df[i, 'identifier'] <- robcheck('identifier', rd[[i]]$asset)
+    df[i, 'units'] <- rd[[i]]$returnInfo['units'][[1]]
+    df[i, 'emv'] <- rd[[i]]$returnInfo['emv'][[1]]
+    df[i, 'returnInfo'] <- rd[[i]]$returnInfo['returnDate'][[1]]
+  }
+  df$pctVal <- df$emv / sum(df$emv, na.rm = TRUE)
+  return(df)
+}
+
+download_sec <- function(long_cik, short_cik, user_email) {
+  doc_type <- 'nport-p'
+  url <- paste0(
+    'https://www.sec.gov/cgi-bin/browse-edgar?action=getcompany&CIK=',
+    long_cik,
+    '&type=',
+    doc_type,
+    '&dateb=&count=5&scd=filings&search_text=')
+  # SEC requires email as a header for authentication, you don't have to register
+  # email on their site, just need valid email address
+  response <- httr::GET(url, add_headers(`User-Agent` = user_email))
+  html_doc <- rvest::read_html(response)
+  tbl <- rvest::html_table(html_doc)
+  # might need to change which table to target if web design changes
+  long_str <- tbl[[3]]$Description[1]
+  file_date <- tbl[[3]]$`Filing Date`[1]
+  # need to extract ##-##-## from table cell to target the url of the latest filing
+  num_id <- stringr::str_extract(long_str, '[[:digit:]]+-[[:digit:]]+-[[:digit:]]+')
+  num_str <- gsub('-', '', num_id)
+  # now we can target url of latest filing and get the holdings
+  url <- paste0('https://www.sec.gov/Archives/edgar/data/',
+                short_cik, '/', num_str, '/primary_doc.xml')
+  response <- httr::GET(url, add_headers(`User-Agent` = user_email))
+  xml_file <- xml2::read_xml(response)
+  doc <- XML::xmlParse(xml_file)
+  root <- XML::xmlRoot(doc)
+  # xmlToList can take ~10+ seconds for large amount of holdings, is there more efficient way?
+  all_list <- XML::xmlToList(root)
+  # current node is invstOrSecs for the holdings
+  all_sec <- all_list$formData$invstOrSecs
+  df <- data.frame(
+    name = NA,
+    lei = NA,
+    title = NA,
+    cusip = NA,
+    isin = NA, # conditional, required to list at least one additional ID
+    altid = NA, # ID is usually ISIN, check for ISIN otherwise call alt
+    balance = NA,
+    units = NA,
+    curCd = NA,
+    exchangeRt = NA, # conditional, if no FX then this field doesn't exist
+    valUSD = NA,
+    pctVal = NA,
+    payoffProfile = NA,
+    assetCat = NA,
+    issuerCat = NA,
+    invCountry = NA,
+    returnInfo = NA
+  )
+  for (i in 1:length(all_sec)) {
+    x <- all_sec[[i]]
+    df[i, 'name'] <- x$name
+    df[i, 'lei'] <- x$lei
+    df[i, 'title'] <- x$title
+    df[i, 'cusip']  <- x$cusip
+    if ('isin' %in% names(x$identifiers)) {
+      df[i, 'isin'] <- x$identifiers$isin
+      df[i, 'altid'] <- NA
+    } else {
+      df[i, 'isin'] <- NA
+      df[i, 'altid'] <- x$identifiers[[1]][2]
+    }
+    df[i, 'balance'] <- x$balance
+    df[i, 'units'] <- x$units
+    if ('curCd' %in% names(x)) {
+      df[i, 'curCd'] <- x$curCd
+      df[i, 'exchangeRt'] <- NA
+    } else if ('currencyConditional' %in% names(x)) {
+      df[i, 'curCd'] <- x$currencyConditional[1]
+      df[i, 'exchangeRt'] <- x$currencyConditional[2]
+    } else {
+      df[i, 'curCd'] <- NA
+      df[i, 'exchangeRt'] <- NA
+    }
+    df[i, 'valUSD'] <- x$valUSD
+    df[i, 'pctVal'] <- x$pctVal
+    df[i, 'payoffProfile'] <- x$payoffProfile
+    if ('assetCat' %in% names(x)) {
+      df[i, 'assetCat'] <- x$assetCat
+    } else if ('assetConditional' %in% names(x)) {
+      df[i, 'assetCat'] <- x$assetConditional[2]
+    } else {
+      df[i, 'assetCat'] <- NA
+    }
+    if ('issuerCat' %in% names(x)) {
+      df[i, 'issuerCat'] <- x$issuerCat[1]
+    } else {
+      df[i, 'issuerCat'] <- NA
+    }
+    df[i, 'invCountry'] <- x$invCountry
+  }
+  df[ ,'returnInfo'] <- as.Date(file_date)
+  df$pctVal <- as.numeric(df$pctVal)
+  df$pctVal <- df$pctVal / 100
+  return(df)
+}
